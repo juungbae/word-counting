f6fd5721614aebec17f3156cb4cba8ccfd54c6b1
juungbae <juungbae@gmail.com>
2021-05-29 20:09:55 +0900

    gitignore 추가..

--- rust/target/debug/deps/rust.d
+++
@@ -1,5 +0,0 @@
-/Users/juwoongbae/study/word-counting/rust/target/debug/deps/rust: src/main.rs
-
-/Users/juwoongbae/study/word-counting/rust/target/debug/deps/rust.d: src/main.rs
-
-src/main.rs:
--- rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-/Users/juwoongbae/study/word-counting/rust/target/debug/rust: /Users/juwoongbae/study/word-counting/rust/src/main.rs
